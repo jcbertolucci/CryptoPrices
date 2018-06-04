609fa7362ef7b330c2315a62a3cc112b6e5b6af3
Julio Cesar Bertolucci <bertolucci1985@gmail.com>
2018-06-04 16:32:41 +1000

    Delete firebaseConfig.js

--- src/database/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  apiKey: "AIzaSyBKS2mTEl_u-L1xWbY9xi3QGVSVT37LCg8",
-  authDomain: "inclitibeta.firebaseapp.com",
-  databaseURL: "https://inclitibeta.firebaseio.com",
-  projectId: "inclitibeta",
-  storageBucket: "inclitibeta.appspot.com",
-  messagingSenderId: '1037154318940'
-}
-export default config
