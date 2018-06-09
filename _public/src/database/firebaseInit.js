0dbc61a52ee81b0a7c744c0f2ed847c58d2fbba7
Julio Cesar Bertolucci <bertolucci1985@gmail.com>
2018-06-09 10:50:49 +1000

    Delete public folder

--- _public/src/database/firebaseInit.js
+++
@@ -1,6 +0,0 @@
-import firebase from 'firebase'
-import 'firebase/firestore' 
-import config from '../database/firebaseConfig'
-
-export const firebaseApp = firebase.initializeApp(config)
-export const firestore = firebaseApp.firestore()
